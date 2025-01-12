b2152c835938490a48bfe03d16e35451f35cf355
deepuchary03 <deepuchary03@gmail.com>
2025-01-12 18:25:47 +0530

    aa

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,16 +1,16 @@
 import React from "react";
-import "./Music.css";
-import "./common.css";
-// Import the CSS file for styling
+import "./Music.css"; // Import the CSS file for styling
+import "./common.css"; // Import the common CSS file
 
 const Music = () => {
   return (
-    <section id="music">
+    <section id="music" className="common-background">
       <h2>Music</h2>
       <div className="music-container">
         <iframe
           className="music-iframe"
           src="https://open.spotify.com/embed/track/3RKywhObU25JLEypRY6HWx?utm_source=generator"
+          title="Spotify Track 1"
           frameBorder="0"
           allowFullScreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
@@ -18,15 +18,8 @@ const Music = () => {
         ></iframe>
         <iframe
           className="music-iframe"
-          src="https://open.spotify.com/embed/track/6wJwkMo278iWVlWl1XQ4WA?utm_source=generator"
-          frameBorder="0"
-          allowFullScreen=""
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        ></iframe>
-        <iframe
-          className="music-iframe"
-          src="https://open.spotify.com/embed/track/4hQ6UGyWQIGJmHSo0J88JW?utm_source=generator"
+          src="https://open.spotify.com/embed/track/4uLU6hMCjMI75M1A2tKUQC?utm_source=generator"
+          title="Spotify Track 2"
           frameBorder="0"
           allowFullScreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
