6b0fde01c8757bc7995f899fa74781b7194313a0
deepuchary03 <deepuchary03@gmail.com>
2025-01-12 18:48:52 +0530

    i

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,8 +6,8 @@ const Gallery = () => {
   return (
     <section id="gallery">
       <h2>Gallery</h2>
-      <div>
-        <img src={g1} alt="Gallery 1" height="400px" width="500px" />
+      <div className="music-container">
+        <img src={g1} alt="Gallery 1" className="music-iframe" />
         <img src="image2.jpg" alt="Gallery 2" />
         <img src="image3.jpg" alt="Gallery 3" />
       </div>
